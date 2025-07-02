0ae3e2f3375e130763a389343e465ab3cc2ab4be
Nataniel44 <josenatanielsoto@gmail.com>
2025-07-02 14:00:04 -0300

    primer cambio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,7 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
-  
 });
